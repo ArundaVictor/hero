80ddfa00924d671c57e2d597c8e6e6245d0ebc39
Arunda Victor <victoroyugi31@gmail.com>
2019-02-03 00:12:00 +0300

    add new methods

--- src/main/java/Squad.java
+++ src/main/java/Squad.java
@@ -5,13 +5,13 @@ public class Squad {
   private String mName;
   private static List<Squad> instances = new ArrayList<Squad>();
   private int mId;
-  private List<Task> mTasks;
+  private List<Hero> mHeroes;
 
   public Squad(String name) {
     mName = name;
     instances.add(this);
     mId = instances.size();
-    mTasks = new ArrayList<Task>();
+    mHeroes = new ArrayList<Heroes>();
   }
 
   public String getName() {
@@ -33,7 +33,7 @@ public class Squad {
    
 
    public List<Hero> getHeroes() {
-    return mTasks;
+    return mHeroes;
   }
 
   public void addHero(Task hero) {
