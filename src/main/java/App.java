c4bb97b638324ea9d6741b3594d42bad2006f44f
Arunda Victor <victoroyugi31@gmail.com>
2019-02-03 00:24:11 +0300

    add a squad form:

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -52,6 +52,21 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+       get("/heroes/:id", (request, response) -> {
+  HashMap<String, Object> model = new HashMap<String, Object>();
+  Hero hero = Hero.find(Integer.parseInt(request.params(":id")));
+  model.put("hero", hero);
+  model.put("template", "templates/hero.vtl");
+  return new ModelAndView(model, layout);
+}, new VelocityTemplateEngine());
+
+       get("/heroes/new", (request, response) -> {
+  Map<String, Object> model = new HashMap<String, Object>();
+  model.put("template", "templates/squad-form.vtl");
+  return new ModelAndView(model, layout);
+}, new VelocityTemplateEngine());
+
+
 
 
 	}
