a09132c797e82875da280c0a89529bfa73612247
Arunda Victor <victoroyugi31@gmail.com>
2019-02-02 19:14:50 +0300

    add main route

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -22,5 +22,12 @@ public class App {
     String layout = "templates/layout.vtl";
 
 
+     get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+
 	}
 }
